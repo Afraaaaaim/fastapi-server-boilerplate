ceb929e6403025adfbadfc6b273466630bdda8be
Afraaaaaim <afraimjoseph@email.com>
2026-06-10 15:32:10 +0530

    made development env changes

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import uvicorn
 from app.core.config import get_settings
+import os
+
+def _is_docker() -> bool:
+    return os.path.exists("/.dockerenv")
 
 if __name__ == "__main__":
     settings = get_settings()
@@ -8,5 +12,5 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=settings.port,
         workers=settings.workers,
-        reload=settings.is_development,
+        reload=settings.is_development and not _is_docker(),
     )
